846914515e44a8a2af3d6a1e3391d27d129d4152
ki733655 <143171590+ki733655@users.noreply.github.com>
2024-03-25 10:03:55 +0530

    refactor

--- src/components/Livestock/pigForms/Piglets.jsx
+++ src/components/Livestock/pigForms/Piglets.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import "./Piglets.css"
 
-const Piglets = () => {
+const Piglet = () => {
   return (
-    <div>Piglets</div>
-  )
-}
+    <>
+      <form action="" className="piglet-form">
+        <div className="mb-3">
+          <label className="form-label">Piglets Id</label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Enter piglet id"
+          ></input>
+          <label className="form-label">Mother Id</label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Enter mother id"
+          ></input>
+          <label className="form-label">Father Id</label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Enter father id"
+          ></input>
+          <label className="form-label">Date of birth</label>
+          <input
+            type="date"
+            className="form-control"
+            id="exampleFormControlInput4"
+          />
+          <label className="form-label">Gender</label>
+          <select className="form-select" aria-label="Default select example">
+            <option selected>Select from the list</option>
+            <option value="1">Sow</option>
+            <option value="2">Boar</option>
+            <option value="3">Khassi</option>
+          </select>
 
-export default Piglets
+          <label className="form-label">Room number</label>
+          <input
+            type="number"
+            className="form-control"
+            id="exampleFormControlInput2"
+            placeholder="Enter room number"
+          ></input>
+          <label className="form-label">Select the date of Swine Fever</label>
+          <input
+            type="date"
+            className="form-control"
+            id="exampleFormControlInput4"
+          />
+          <label className="form-label">Select the date of Deworming</label>
+          <input
+            type="date"
+            className="form-control"
+            id="exampleFormControlInput5"
+          />
+          <label className="form-label">Weight</label>
+          <input
+            type="number"
+            className="form-control"
+            id="exampleFormControlInput6"
+            placeholder="in kgs"
+          />
+          <input type="submit" className="btn btn-primary" value="Submit" />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Piglet;
--- src/components/Livestock/pigForms/Sow.jsx
+++ src/components/Livestock/pigForms/Sow.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
-
+import "./Sow.css";
 const Sow = () => {
   return (
-    <div>Sow</div>
-  )
-}
+    <>
+      <form action="" className="sow-form">
+        <div className="mb-3">
+          <label className="form-label">Sow Id</label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Enter sow id"
+          ></input>
 
-export default Sow
+          <label className="form-label">Room number</label>
+          <input
+            type="number"
+            className="form-control"
+            id="exampleFormControlInput2"
+            placeholder="Enter room number"
+          ></input>
+          <label className="form-label">Select the date of CSF</label>
+          <input
+            type="date"
+            className="form-control"
+            id="exampleFormControlInput3"
+          />
+          <label className="form-label">Select the date of FMD</label>
+          <input
+            type="date"
+            className="form-control"
+            id="exampleFormControlInput4"
+          />
+          <label className="form-label">Select the date of Deworn</label>
+          <input
+            type="date"
+            className="form-control"
+            id="exampleFormControlInput5"
+          />
+          <label className="form-label">Weight</label>
+          <input
+            type="number"
+            className="form-control"
+            id="exampleFormControlInput6"
+            placeholder="in kgs"
+          />
+          <input type="submit" className="btn btn-primary" value="Submit" />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Sow;
