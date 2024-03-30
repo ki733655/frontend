bd0767bd5822fc124ca4de9ea38a353184a854bf
ki733655 <143171590+ki733655@users.noreply.github.com>
2024-03-30 22:11:18 +0530

    boar detail refactor

---
+++ src/components/Livestock/pigdetail/Boar/BoarEditForm.jsx
@@ -0,0 +1,129 @@
+import { useState } from "react";
+import "./BoarEditForm.css";
+import { MdCancel } from "react-icons/md";
+import axios from "axios"; // Import Axios for making HTTP requests
+
+const BoarEditForm = ({ editItem, setEditItem }) => {
+    if (!editItem) return null;
+  
+    const initialFormData = {
+      id: editItem.id || "",
+      roomNumber: editItem.roomNumber || "",
+      CSF: editItem.CSF || "",
+      FMD: editItem.FMD || "",
+      Weight: editItem.Weight || "",
+    };
+  
+    const [formData, setFormData] = useState(initialFormData);
+  
+    const handleChange = (e) => {
+      const { id, value } = e.target;
+      setFormData((prevData) => ({
+        ...prevData,
+        [id]: value,
+      }));
+    };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      // Assuming your server endpoint is expecting the edited data as JSON in the request body
+      await axios.put("http://localhost:3000/boar-edit", formData);
+      alert("Data updated successfully");
+      // Clear the form data after successful submission
+      setFormData({
+        id: "",
+        roomNumber: "",
+        CSF: "",
+        FMD: "",
+        Deworm: "",
+        Weight: "",
+      });
+      // Reset editItem to null to exit edit mode
+      setEditItem(null);
+      // Fetching the updated data after deletion
+      
+      
+    } catch (error) {
+      // Handle error if the request fails
+      console.error("Error occurred:", error);
+    }
+  };
+
+  
+
+  return (
+    <>
+      <div className="boar-form">
+        <div className="boar-form-container">
+          <div className="pig-header">
+            <h4 style={{ textAlign: "center" }}>Edit pig details</h4>
+            <MdCancel style={{fontSize: "4vh"}} onClick={() => setEditItem(null)} />
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label className="form-label">Boar Id</label>
+              <input
+                type="text"
+                className="form-control"
+                id="id"
+                placeholder="Enter boar id"
+                value={formData.id}
+                onChange={handleChange}
+              />
+
+              <label className="form-label">Room number</label>
+              <input
+                type="number"
+                className="form-control"
+                id="roomNumber"
+                placeholder="Enter room number"
+                value={formData.roomNumber}
+                onChange={handleChange}
+              />
+
+              <label className="form-label">Select the date of CSF</label>
+              <input
+                type="date"
+                className="form-control"
+                id="CSF"
+                value={formData.CSF}
+                onChange={handleChange}
+              />
+              <label className="form-label">Select the date of FMD</label>
+              <input
+                type="date"
+                className="form-control"
+                id="FMD"
+                value={formData.FMD}
+                onChange={handleChange}
+              />
+              <label className="form-label">Select the date of Deworm</label>
+              <input
+                type="date"
+                className="form-control"
+                id="Deworm"
+                value={formData.Deworm}
+                onChange={handleChange}
+              />
+              <label className="form-label">Weight</label>
+              <input
+                type="number"
+                className="form-control"
+                id="Weight"
+                placeholder="in kgs"
+                value={formData.Weight}
+                onChange={handleChange}
+              />
+
+              <input type="submit" className="btn btn-primary" id="boar-edit-submit" value="Save" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BoarEditForm;
