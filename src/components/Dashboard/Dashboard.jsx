2973f83969d8c6c62eb3ccc34da538407211e76f
ki733655 <143171590+ki733655@users.noreply.github.com>
2024-04-29 14:38:03 +0530

    kaam

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./Dashboard.css"
+import Box from '@mui/material/Box';
+
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <>
+      <div className="dash-main">
+        <div className="dash-top">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+        </div>
+        <div className="dash-bottom">
+
+        </div>
+      </div>
+    </>
   )
 }
 
